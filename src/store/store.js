b77df0200ff195ae7f2fa4b24d066bdd2e4c3a11
Eric Peterik <epeterik@hotmail.com>
2018-02-09 23:24:05 -0600

    updated file with image URLs

--- src/store/store.js
+++ src/store/store.js
@@ -7,13 +7,13 @@ import logger from 'redux-logger';
 import reducer from '../reducers/reducer';
 
 const initialState = {
-    cityList: [{cityName: 'Seatle, Wa', imageUrl: ''},
-               {cityName: 'San Jose, Ca', imageUrl: ''}, 
-               {cityName: 'Burbank, Ca', imageUrl: ''},
-               {cityName: 'Dallas, Tx', imageUrl: ''},
-               {cityName: 'Washington, DC', imageUrl: ''},
-               {cityName: 'Chicago, Il', imageUrl: ''}, 
-               {cityName: 'Tulsa, OK', imageUrl: ''}],
+    cityList: [{cityName: 'Seatle, Wa', imageUrl: 'https://images.pexels.com/photos/656195/pexels-photo-656195.jpeg?h=350&auto=compress&cs=tinysrgb'},
+               {cityName: 'San Jose, Ca', imageUrl: 'https://images.pexels.com/photos/7653/pexels-photo.jpeg?h=350&auto=compress&cs=tinysrgb'}, 
+               {cityName: 'Burbank, Ca', imageUrl: 'https://images.pexels.com/photos/462331/pexels-photo-462331.jpeg?h=350&auto=compress&cs=tinysrgb'},
+               {cityName: 'Dallas, Tx', imageUrl: 'https://images.pexels.com/photos/280193/pexels-photo-280193.jpeg?h=350&auto=compress&cs=tinysrgb'},
+               {cityName: 'Washington, DC', imageUrl: 'https://images.pexels.com/photos/208702/pexels-photo-208702.jpeg?h=350&auto=compress&cs=tinysrgb'},
+               {cityName: 'Chicago, Il', imageUrl: 'https://images.pexels.com/photos/167200/pexels-photo-167200.jpeg?h=350&auto=compress&cs=tinysrgb'}, 
+               {cityName: 'Tulsa, OK', imageUrl: 'https://www.twenty20.com/photos/ecbf59fb-1888-4ec7-b167-694de4bb80bd?cta=ft&cta_uid=f4fa0815-85e9-48e6-a02e-d08d3d1fbcf0&utm_t20_source=partner&utm_t20_channel=pexels&utm_t20_campaign=related'}],
     currentCityData: {}
 }
 
